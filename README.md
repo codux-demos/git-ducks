37c80e61a05c8decadfdc52dfc049fbfd472f8c0
cario <cario@wix.com>
2023-01-30 18:17:47 +0200

    base

---
+++ README.md
@@ -0,0 +1,16 @@
+<div align="center">  
+    <img height="50"src="./src/assets/codux.svg">  
+    <h1 >git ducks</h1>
+</div>
+
+### A Visual IDE for React Projects
+
+Codux is an all-in-one visual development environment. Whether you’re starting a new app, creating components, or editing an existing project, every visual change you make reflects instantly in the code and vice versa. To learn more about Codux, visit our website - [https://www.codux.com/](https://www.codux.com/)
+
+### Take it for a Spin
+
+This repo provides an interactive playground for Blueprint UI toolkit. It's a great way to explore how the library and Codux work.
+
+-   Learn how to edit, render and compose apps that make use of [@blueprintjs/core](https://blueprintjs.com/docs/#core) components.
+-   Create React-based components with TypeScript support.
+-   Visually edit in real-time and in an isolated environment.
---
+++ src/test-toolkit/mocks/mock-user.ts
@@ -0,0 +1,36 @@
+import { UserResponse } from '../../model/api-user';
+
+export const response_user: UserResponse = {
+    user: {
+        avatarUrl:
+            'https://static.wixstatic.com/media/68d3a9_a9372f08e00c4a44ab575bbdd42a51ee~mv2.png',
+        bio: null,
+        login: 'AnnieW',
+        name: 'Annie Williams',
+        location: 'Boston MA USA',
+        email: 'anniew@codux.com',
+        twitterUsername: null,
+        websiteUrl: 'http://codux.com/anniewilliams',
+        techs: {
+            technologies: ['react', 'html', 'node', 'python', 'js', 'csharp'],
+        },
+        company: 'Codux',
+        following: {
+            totalCount: 68,
+        },
+        followers: {
+            totalCount: 4319,
+        },
+        status: null,
+        organizations: {
+            edges: [
+                {
+                    node: {
+                        login: 'wix',
+                        avatarUrl: 'https://i.pravatar.cc/150?img=wix',
+                    },
+                },
+            ],
+        },
+    },
+};
