a5bf4969a5a359a80f6f0b9c82a079e571ca8cd9
Ari Kishon <ariki@wix.com>
2023-03-05 12:05:48 +0200

    initial commit

---
+++ src/_codux/boards/top-bar/top-bar.board.tsx
@@ -0,0 +1,30 @@
+import { createBoard } from '@wixc3/react-board';
+import { TopBar } from '../../../components/pages/top-bar/top-bar';
+import type { ApiUser } from '../../../model/api-user';
+
+const JohnDoe: ApiUser = {
+    avatar: 'https://images.unsplash.com/photo-1531746020798-e6953c6e8e04?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80',
+    bio: 'Some bio information',
+    username: 'jane-doe',
+    location: 'USA',
+    name: 'Jane Doe',
+    email: 'jane@doe.com',
+    followers: 19,
+    following: 20,
+    twitter: '',
+    url: '',
+    organization: '',
+    organizations: [],
+    techs: {
+        technologies: ['js', 'node', 'react', 'python'],
+    },
+};
+
+export default createBoard({
+    name: 'Top Bar',
+    Board: () => <TopBar user={JohnDoe} />,
+    environmentProps: {
+        windowHeight: 452,
+        windowWidth: 1352,
+    },
+});
